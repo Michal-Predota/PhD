b38a1aab7b463eb2528cd5a6f220e7fdd61bee2f
Michal-Predota <michalpredota123@gmail.com>
2026-08-19 15:10:45 +0200

    more tests for pi+ mixing

--- fit.C
+++ fit.C
@@ -871,7 +871,11 @@ int fit()
 //	TFile *f = new TFile("./jobs/smash/pureSmash_pip_analysis4.5GeV_buffer20_TotalPositiveAndNegativeChargeMix_nTracksGT1.root","read");
 //	TFile *f = new TFile("./jobs/smash/pureSmash_pip_analysis4.5GeV_buffer20_nPipMix_nTracksGT1.root","read");
 //	TFile *f = new TFile("./jobs/smash/pureSmash_pip_analysis4.5GeV_buffer20_positiveAndNegativePionsMix_nTracksGT1.root","read");
-	TFile *f = new TFile("./jobs/smash/pureSmash_pip_analysis4.5GeV_buffer20_totalMomentumMix_nTracksGT1.root","read");
+//	TFile *f = new TFile("./jobs/smash/pureSmash_pip_analysis4.5GeV_buffer20_totalMomentumMix_nTracksGT1.root","read");
+//	TFile *f = new TFile("./jobs/smash/pureSmash_pip_analysis4.5GeV_buffer20_totalMomentumAndChargedTracksMix_nTracksGT1.root","read");
+//	TFile *f = new TFile("./jobs/smash/pureSmash_pip_analysis4.5GeV_buffer20_totalKtAndRapidityMix_nTracksGT1.root","read");
+	TFile *f = new TFile("./jobs/smash/pureSmash_pip_analysis4.5GeV_buffer20_totalKtMix_nTracksGT1.root","read");
+//	TFile *f = new TFile("./jobs/smash/pureSmash_pip_analysis4.5GeV_buffer20_totalRapidityMix_nTracksGT1.root","read");
 	
 	
 	
